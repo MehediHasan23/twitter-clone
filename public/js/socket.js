e4821087905aab7fa6b3562d8904105b790985e0
MehediHasan23 <developer.mehedi23@gmail.com>
2022-12-26 13:38:39 +0600

    fixed some bugs

--- public/js/socket.js
+++ public/js/socket.js
@@ -5,6 +5,7 @@ const socket = io("http://localhost:3005/");
 
 socket.emit("setup", user);
 
+/* established a connection */
 socket.on("connected", () => {
   isConnected = true;
   console.log(isConnected);
--- socektServer.js
+++ socektServer.js
@@ -3,18 +3,23 @@ const http = require("http");
 const httpSocketServer = http.createServer();
 
 const io = new Server(httpSocketServer, {
-  cors: `http://localhost:3000/`,
+  cors: ["http://localhost:3000/"],
+
   method: ["GET", "POST", "PUT", "DELETE"],
 });
 
+/* established socket connection on server site */
 io.on("connection", socket => {
   socket.on("setup", user => {
-    socket.join(user._id);
-    io.to(user._id).emit("connected");
-    console.log(user.firstName + " connected");
+    /* connect a new socket user in a room by his[database._id] */
+    const userId = user._id;
+    socket.join(userId);
+    socket.emit("connected");
+    console.log(user.firstName, " connected");
 
+    /* disconnect a user */
     socket.on("disconnect", () => {
-      console.log(user.firstName + " disconnected");
+      console.log(user.firstName, " disconnected");
     });
   });
 });
